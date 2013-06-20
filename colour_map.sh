b720c79040ccccc7dd31db60e41dcf988cf85018
ncat <ncat0526@gmail.com>
2013-06-20 20:27:41 +0800

    Start building color map for bash

---
+++ colour_map.sh
@@ -0,0 +1,16 @@
+
+
+
+function Colour.printall(){
+  for COLOR in $(seq 0 255) 
+  do
+    for STYLE in "38;5"
+    do
+      TAG="\033[${STYLE};${COLOR}m"
+      STR="${STYLE};${COLOR}"
+      echo -ne "${TAG}${STR}${NONE}  "
+    done
+    echo
+  done
+}
+
